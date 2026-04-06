0f9c53cf89f398366049c65d4eb9ce65e63a9dbe
narender-p <narender.dev.work@gmail.com>
2026-04-06 11:38:28 +0530

    log crate

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use env_logger::Env;
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
 use sqlx::PgPool;
 use std::net::TcpListener;
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     // Renamed!
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
