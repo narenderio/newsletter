6def8961903b96b5eed7ffc5c17c800b8f4d2a6e
narender-p <narender.dev.work@gmail.com>
2026-04-08 11:54:00 +0530

    tracing crate

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-use env_logger::Env;
+//! src/main.rs
 use newsletter::configuration::get_configuration;
 use newsletter::startup::run;
-use sqlx::PgPool;
+use newsletter::telemetry::{get_subscriber, init_subscriber};
+use secrecy::ExposeSecret;
+use sqlx::postgres::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // `init` does call `set_logger`, so this is all we need to do.
-    // We are falling back to printing all logs at info-level or above
-    // if the RUST_LOG environment variable has not been set.
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let subscriber = get_subscriber("newsltter".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     // Renamed!
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,29 +9,28 @@ pub struct FormData {
     pub name: String,
 }
 
-pub async fn subscribe(
-    form: web::Form<FormData>,
-    // Retrieving a connection from the application state!
-    pool: web::Data<PgPool>,
-) -> HttpResponse {
-    // Let's generate a random unique identifier
-    let request_id = Uuid::new_v4();
-    log::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
-    );
-    log::info!(
-        "request_id {} - Saving new subscriber details in the database",
-        request_id
-    );
-    // `Result` has two variants: `Ok` and `Err`.
-    // The first for successes, the second for failures.
-    // We use a `match` statement to choose what to do based
-    // on the outcome.
-    // We will talk more about `Result` going forward!
-    match sqlx::query!(
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+    request_id = %Uuid::new_v4(),
+    subscriber_email = %form.email,
+    subscriber_name = %form.name
+    )
+)]
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -41,25 +40,14 @@ pub async fn subscribe(
         form.name,
         Utc::now()
     )
-    // wrapped by `web::Data`.
-    // Using the pool as a drop-in replacement
-    .execute(pool.get_ref())
+    .execute(pool)
     .await
-    {
-        Ok(_) => {
-            log::info!(
-                "request_id {} - New subscriber details have been saved",
-                request_id
-            );
-            HttpResponse::Ok().finish()
-        }
-        Err(e) => {
-            log::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+        // Using the `?` operator to return early
+        // if the function failed, returning a sqlx::Error
+        // We will talk about error handling in depth later!
+    })?;
+    Ok(())
 }
